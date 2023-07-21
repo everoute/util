87d1218efb259d0abb25563bb1a979b66c1a6516
eustrainLee <eustrain@outlook.com>
2023-07-21 04:14:22 +0000

    feat(expected): add expected lib
    Same as https://github.com/everoute/analyzer/pull/148

---
+++ expected.go
@@ -0,0 +1,197 @@
+package expected
+
+import "errors"
+
+type state int
+
+const (
+	hasNothing state = iota // default
+	hasValue
+	hasError
+)
+
+var (
+	errNilPointer error = errors.New("nil pointer")
+)
+
+// An Expected object refers to a valid value or an error
+type Expected[T any] struct {
+	value T
+	err   error
+	state state
+}
+
+// Do not create an empty Expected object unless it will never be used.
+func Empty[T any]() Expected[T] {
+	return Expected[T]{
+		err:   nil,
+		state: hasNothing,
+	}
+}
+
+// Create an Expected object from return value.
+func Pack[T any](value T, err error) Expected[T] {
+	if err != nil {
+		return Expected[T]{
+			err:   err,
+			state: hasError,
+		}
+	}
+	return Expected[T]{
+		value: value,
+		state: hasValue,
+	}
+}
+
+// TODO: Add support for interface{...}
+// Create an Expected object from return value and the value is not a nil pointer.
+func PackNonNilP[P *B, B any](value P, err error) Expected[P] {
+	if err != nil {
+		return Expected[P]{
+			err:   err,
+			state: hasError,
+		}
+	}
+	if value == nil {
+		return Expected[P]{
+			err:   errNilPointer,
+			state: hasError,
+		}
+	}
+	return Expected[P]{
+		value: value,
+		state: hasValue,
+	}
+}
+
+func Value[T any](value T) Expected[T] {
+	return Expected[T]{
+		value: value,
+		state: hasValue,
+	}
+}
+
+// TODO: Add support for interface{...}
+func NonNilP[T any](value *T) Expected[*T] {
+	if value == nil {
+		return Expected[*T]{
+			err:   errNilPointer,
+			state: hasError,
+		}
+	}
+	return Expected[*T]{
+		value: value,
+		state: hasValue,
+	}
+}
+
+// An undefined behavior will occur if you passed a nil pointer.
+func Error[T any](err error) Expected[T] {
+	return Expected[T]{
+		err:   err,
+		state: hasError,
+	}
+}
+
+func Wrap[Arg, Res any](fn func(Arg) (Res, error)) func(Arg) Expected[Res] {
+	return func(arg Arg) Expected[Res] {
+		return Pack(fn(arg))
+	}
+}
+
+// TODO: Add support for interface{...}
+func WrapNonNilP[Arg any, Res *B, B any](fn func(Arg) (Res, error)) func(Arg) Expected[Res] {
+	return func(arg Arg) Expected[Res] {
+		return PackNonNilP(fn(arg))
+	}
+}
+
+// Expect genericity begin go 1.18
+func Unwrap[Arg, Res any](fn func(Arg) Expected[Res]) func(Arg) (Res, error) {
+	return func(arg Arg) (Res, error) {
+		return fn(arg).Get()
+	}
+}
+
+func (e *Expected[T]) SetValue(value T) {
+	e.value = value
+	e.state = hasValue
+}
+
+func (e *Expected[T]) SetError(err error) {
+	e.err = err
+	e.state = hasError
+}
+
+func (e Expected[T]) Value() (T, bool) {
+	return e.value, e.state == hasValue
+}
+
+func (e Expected[T]) Error() error {
+	if e.state == hasError {
+		return e.err
+	}
+	return nil
+}
+
+// TODO: release parameter type restrictions
+func (e Expected[T]) ValueOr(oth T) T {
+	if e.state == hasValue {
+		return e.value
+	}
+	return oth
+}
+
+func (e Expected[T]) Get() (T, error) {
+	return e.value, e.err
+}
+
+func (e Expected[T]) IsOk() bool {
+	return e.state == hasValue
+}
+
+func (e Expected[T]) IsBad() bool {
+	return e.state == hasError
+}
+
+// TODO: release parameter type restrictions
+// call fn if it has a value
+func (e Expected[T]) AndThen(fn func(T) Expected[T]) Expected[T] {
+	if e.state == hasValue {
+		return fn(e.value)
+	}
+	return e
+}
+
+// TODO: release parameter type restrictions
+// call fn if it has not a value
+func (e Expected[T]) OrElse(fn func(error) Expected[T]) Expected[T] {
+	if e.state == hasError {
+		return fn(e.err)
+	}
+	return e
+}
+
+// TODO: release parameter type restrictions
+func (e Expected[T]) Or(res Expected[T]) Expected[T] {
+	if e.state == hasValue {
+		return e
+	}
+	return res
+}
+
+// TODO: release parameter type restrictions
+func (e Expected[T]) Transform(fn func(T) T) Expected[T] {
+	if e.state == hasValue {
+		return Value(fn(e.value))
+	}
+	return e
+}
+
+// TODO: release parameter type restrictions
+func (e Expected[T]) TransformError(fn func(error) error) Expected[T] {
+	if e.state == hasError {
+		return Error[T](fn(e.err))
+	}
+	return e
+}
---
+++ expected_test.go
@@ -0,0 +1,142 @@
+package expected_test
+
+import (
+	"errors"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/everoute/expected"
+)
+
+func TestValue(t *testing.T) {
+	RegisterTestingT(t)
+	var value int = 1
+	e := expected.Value(value)
+	v, err := e.Get()
+	Expect(err).Should(Succeed())
+	Expect(v).Should(Equal(value))
+	v, ok := e.Value()
+	Expect(ok).Should(BeTrue())
+	Expect(v).Should(Equal(value))
+	err = e.Error()
+	Expect(err).Should(Succeed())
+}
+
+func TestError(t *testing.T) {
+	RegisterTestingT(t)
+	var demoErr = errors.New("demo error")
+	e := expected.Error[int](demoErr)
+	v, err := e.Get()
+	Expect(v).Should(Equal(0))
+	Expect(err).ShouldNot(Succeed())
+	v, ok := e.Value()
+	Expect(v).Should(Equal(0))
+	Expect(ok).Should(BeFalse())
+	err = e.Error()
+	Expect(err).Should(Equal(demoErr))
+}
+
+func TestValueOr(t *testing.T) {
+	RegisterTestingT(t)
+	var v int
+	v = expected.Value(1).ValueOr(2)
+	Expect(v).Should(Equal(1))
+
+	v = expected.Error[int](errors.New("demo error")).ValueOr(2)
+	Expect(v).Should(Equal(2))
+}
+
+func TestAndThenWithValue(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Value(1)
+	e = e.AndThen(func(i int) expected.Expected[int] {
+		return expected.Value(i + 1)
+	})
+	v, err := e.Get()
+	Expect(err).Should(Succeed())
+	Expect(v).Should(Equal(1 + 1))
+	e = e.AndThen(func(i int) expected.Expected[int] {
+		return expected.Error[int](errors.New("demo error"))
+	})
+	v, err = e.Get()
+	Expect(err).ShouldNot(Succeed())
+	Expect(v).ShouldNot(Equal(1 + 1))
+}
+
+func TestAndThenWithError(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Error[int](errors.New("demo error"))
+	e.AndThen(func(i int) expected.Expected[int] {
+		return expected.Value(i + 1)
+	})
+	err := e.Error()
+	Expect(err).ShouldNot(Succeed())
+}
+
+func TestOrElseWithValue(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Value(1)
+	err := e.OrElse(func(err error) expected.Expected[int] {
+		t.Fail()
+		return expected.Error[int](err)
+	}).Error()
+	Expect(err).Should(Succeed())
+}
+
+func TestOrElseWithError(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Error[int](errors.New("demo error"))
+	e = e.OrElse(func(err error) expected.Expected[int] {
+		return expected.Error[int](errors.New("another error"))
+	})
+	err := e.Error()
+	Expect(err).ShouldNot(Succeed())
+	e = e.OrElse(func(err error) expected.Expected[int] {
+		return expected.Value(1)
+	})
+	v, err := e.Get()
+	Expect(err).Should(Succeed())
+	Expect(v).Should(Equal(1))
+}
+
+func TestTransformWithValue(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Value(1)
+	e = e.Transform(func(i int) int {
+		return i + 1
+	})
+	v, err := e.Get()
+	Expect(err).Should(Succeed())
+	Expect(v).Should(Equal(1 + 1))
+}
+
+func TestTransformWithError(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Error[int](errors.New("demo error"))
+	e.Transform(func(i int) int {
+		return i + 1
+	})
+	err := e.Error()
+	Expect(err).ShouldNot(Succeed())
+}
+
+func TestTransformErrorWithValue(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Value(1)
+	err := e.TransformError(func(err error) error {
+		t.Fail()
+		return err
+	}).Error()
+	Expect(err).Should(Succeed())
+}
+
+func TestTransformErrorWithError(t *testing.T) {
+	RegisterTestingT(t)
+	e := expected.Error[int](errors.New("demo error"))
+	e = e.TransformError(func(err error) error {
+		return errors.New("another error")
+	})
+	err := e.Error()
+	Expect(err).ShouldNot(Succeed())
+}
